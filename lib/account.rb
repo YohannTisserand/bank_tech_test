87678e688053fbe089b1afd189729876fb446234
YohannTisserand <tisserand.yohann@gmail.com>
2021-06-02 08:54:13 +0100

    refactor

--- lib/account.rb
+++ lib/account.rb
@@ -4,13 +4,14 @@ class Account
   DEFAULT_BALANCE = 0
   attr_accessor :balance
 
-  def initialize( balance = DEFAULT_BALANCE, transaction_class: Transaction)
-    @balance = balance
+  def initialize(transaction_class: Transaction)
+    @balance = DEFAULT_BALANCE
     @transaction_class = transaction_class
     @transaction_history = []
   end
 
   def deposit(credit)
+    low_deposit(credit)
     @transaction_history << @balance += credit
   end
 
@@ -19,13 +20,13 @@ class Account
     @transaction_history <<  @balance -= debit
   end
 
-  def header
-    'date || credit || debit || balance'
-  end
-
   private
 
   def low_funds(debit)
     raise "Not enough funds" unless @balance - debit >= DEFAULT_BALANCE
+  end
+
+  def low_deposit(credit)
+    raise "must be at least £1" if credit == DEFAULT_BALANCE
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,27 +1,36 @@
 require 'account'
 
 describe Account do
-  it 'should start empty' do
-    expect(subject.balance).to eq Account::DEFAULT_BALANCE
+
+  context '#initialize' do
+    it 'should start empty' do
+      expect(subject.balance).to eq Account::DEFAULT_BALANCE
+    end
   end
 
-  it 'should change if a deposit has been made' do
-    subject.deposit(200)
-    expect(subject.balance).to eq 200
+  context '#deposit' do
+    it 'should change if a deposit has been made' do
+      subject.deposit(200)
+      expect(subject.balance).to eq 200
+    end
   end
 
-  it 'should change if a withdraw has been made' do
-    subject.deposit(200)
-    subject.withdraw(100)
-    expect(subject.balance).to eq 100
+  context 'withdraw' do
+    it 'should change if a withdraw has been made' do
+      subject.deposit(200)
+      subject.withdraw(100)
+      expect(subject.balance).to eq 100
+    end
   end
 
-  it 'raises an error if withdraw without enough funds' do
-    subject.deposit(100)
-    expect { subject.withdraw(200) }.to raise_error "Not enough funds"
-  end
+  context '#raise_errors' do
+    it 'raises an error if withdraw without enough funds' do
+      subject.deposit(100)
+      expect { subject.withdraw(200) }.to raise_error "Not enough funds"
+    end
 
-  it 'prints the header' do
-    expect(subject.header).to eq 'date || credit || debit || balance'
+    it 'raises an error if depositis nil' do
+      expect { subject.deposit(0) }.to raise_error "must be at least £1"
+    end
   end
 end
