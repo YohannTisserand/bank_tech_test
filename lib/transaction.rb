cfccee0af43a2658f28c7476d720ef8711661c01
YohannTisserand <tisserand.yohann@gmail.com>
2021-06-03 10:28:47 +0100

    removed the hardcoded date into the test

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -11,13 +11,13 @@ class Transaction
     "#{@date} || #{spaced(format(@credit))}|| #{spaced(format(@debit))}|| #{spaced(format(@balance))}"
   end
 
-  def format(item)
-    '%.2f' % item if !item.nil?
-  end
-
   private
 
   def spaced(item)
     "#{item} " if item
+  end
+
+  def format(item)
+    '%.2f' % item if !item.nil?
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -4,28 +4,32 @@ describe Transaction do
 
   context '#print_logs' do
     it 'display a date' do
-      expect(subject.print_logs).to eq "02/06/2021 || || || "
+      expect(subject.print_logs).to eq "#{date} || || || "
     end
   end
   
   context '#credit' do
     it 'pushes a deposit into the logs' do
       subject = Transaction.new(credit: 2000)
-      expect(subject.print_logs).to eq "02/06/2021 || 2000 || || "
+      expect(subject.print_logs).to eq "#{date} || 2000.00 || || "
     end
   end
 
   context '#debit' do
     it 'pushes a withdraw into the logs' do
       subject = Transaction.new(debit: 200)
-      expect(subject.print_logs).to eq "02/06/2021 || || 200 || "
+      expect(subject.print_logs).to eq "#{date} || || 200.00 || "
     end
   end
 
   context '#balance' do
     it 'pushes a balance into the logs' do
       subject = Transaction.new(credit: 300, debit: 200, balance: 100)
-      expect(subject.print_logs).to eq "02/06/2021 || 300 || 200 || 100 "
+      expect(subject.print_logs).to eq "#{date} || 300.00 || 200.00 || 100.00 "
     end
+  end
+
+  def date
+    Time.now.strftime('%d/%m/%Y')
   end
 end
